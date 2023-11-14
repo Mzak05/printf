8e8f8fd55429af438e28e888c73b5e294baf5c2a
Mzak05 <mzakyassir@.com>
2023-11-14 09:39:20 -0800

    set

--- _printf.c
+++ _printf.c
@@ -1,48 +1,46 @@
 #include "main.h"
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
-int pop(char *c);
+* _printf - Printf function
+* @format: format.
+* Return: Printed chars.
+*/
 int _printf(const char *format, ...)
 {
-int c;
-char t;
-char *st;
 va_list list;
+int c, i, j;
 c = 0;
-if (format == NULL)
-	return (-1);
+if (!format)
+return (-1);
+
 va_start(list, format);
-while (*format)
+for (i = 0; format[i]; i++)
 {
-if (*format != '%')
-	c += write(1, format, 1);
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '%')
+c += write(1, &format[i], 1);
+
+else if (format[i] == 'c')
+{
+char ch = va_arg(list, int);
+c += write(1, &ch, 1);
+}
+else if (format[i] == 's')
+{
+const char *t = va_arg(list, const char *);
+if (t)
+for (j = 0; t[j]; j++)
+c += write(1, &t[j], 1);
+}
 else
 {
-format++;
-if (*format == '\0')
-break;
-switch (*format)
-{
-case '%':
-	c += write(1, "%", 1);
-	break;
-case 'c':
-t = va_arg(list, int);
-c += write(1, &t, 1);
-break;
-case 's':
-	st = va_arg(list, char *);
-	c += pop(st);
-break;
-default:
+va_end(list);
 return (-1);
-break;
 }
 }
-format++;
+else
+c += write(1, &format[i], 1);
 }
 va_end(list);
 return (c);

9e53fbd6e16182717093d4e1a47e251e1817a193
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-14 12:52:12 -0800

    task1 with crossed fingers again

--- _printf.c
+++ _printf.c
@@ -1,5 +1,48 @@
 #include "main.h"
 /**
+ * _inttoASC - converts an integer into a string
+ * @AC: buffer where result is stored
+ * @number: an integer that we should convert
+ * Return: the length of the AC string
+*/
+int _inttoASC(int number, char *AC)
+{
+	int i = 0, first, last;
+
+	if (number == 0)
+	{
+		AC[i] = '0';
+		AC[++i] = '\0';
+		return (1);
+	}
+	if (number < 0)
+	{
+		AC[i++] = '-';
+		number = (-number);
+	}
+	while (number != 0)
+	{
+		int rest = number % 10;
+
+		AC[i++] = rest + '0';
+		number = number / 10;
+	}
+	AC[i] = '\0';
+	first = 0;
+	last = (i - 1);
+	while (first < last)
+	{
+		char tomp = AC[first];
+
+		AC[first] = AC[last];
+		AC[last] = tomp;
+		first++;
+		last--;
+	}
+	return (i);
+}
+
+/**
 * _printf - Printf function
 * @format: format.
 * Return: Printed chars.
@@ -34,6 +77,14 @@ const char *t = va_arg(list, const char *);
 if (t)
 for (j = 0; t[j]; j++)
 c += write(1, &t[j], 1);
+}
+else if (format[i] == 'd' || format[i] == 'i')
+{
+	int num = va_arg(list, int);
+	char buffer[20];
+	int L = _inttoASC(num, buffer);
+
+	c += write(1, buffer, L);
 }
 else
 {

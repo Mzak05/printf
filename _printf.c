bac27e6d8a00395dd8e8c17bfa60a2d91d76e69b
Mzak05 <mzakyassir@.com>
2023-11-14 10:58:25 -0800

    set

--- _printf.c
+++ _printf.c
@@ -6,43 +6,42 @@
 */
 int _printf(const char *format, ...)
 {
-int printed_chars = 0;
-va_list args;
-va_start(args, format);
-while (*format)
+va_list list;
+int c, i, j;
+c = 0;
+if (!format)
+return (-1);
+
+va_start(list, format);
+for (i = 0; format[i]; i++)
 {
-if (*format != '%')
-printed_chars += write(1, format, 1); 
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '%')
+c += write(1, &format[i], 1);
+
+else if (format[i] == 'c')
+{
+char ch = va_arg(list, int);
+c += write(1, &ch, 1);
+}
+else if (format[i] == 's')
+{
+const char *t = va_arg(list, const char *);
+if (t)
+for (j = 0; t[j]; j++)
+c += write(1, &t[j], 1);
+}
 else
 {
-format++;
-if (*format == 'c')
-{
-char c = va_arg(args, int);
-printed_chars += write(1, &c, 1);
+c += write(1, "%", 1);
+c += write(1, &format[i], 1);
 }
-else if (*format == 's')
-{
-char *str = va_arg(args, char*);
-while (*str)
-{
-printed_chars += write(1, str, 1);
-str++;
-}
-}
-else if (*format == '%')
-{
-printed_chars += write(1, format, 1);
 }
 else
-{
-write(1, "%", 1);
-write(1, format, 1);
-printed_chars += 2;
+c += write(1, &format[i], 1);
 }
-}
-format++;
-}
-va_end(args);
-return (printed_chars);
+va_end(list);
+return (c);
 }

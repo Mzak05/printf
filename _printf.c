77ec12d198fd36667b78fd1bc7c2a2b10de35b30
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-13 17:19:10 -0800

    task0

--- _printf.c
+++ _printf.c
@@ -6,13 +6,13 @@
 */
 int _printf(const char *format, ...)
 {
+int i, c, j;
 va_list list;
-int c = 0;
+c = 0;
 if (!format)
 return (-1);
-
 va_start(list, format);
-for (int i = 0; format[i]; i++)
+for (i = 0; format[i]; i++)
 {
 if (format[i] == '%')
 {
@@ -24,7 +24,6 @@ return (c);
 }
 if (format[i] == '%')
 c += write(1, &format[i], 1);
-
 else if (format[i] == 'c')
 {
 char ch = va_arg(list, int);
@@ -34,7 +33,7 @@ else if (format[i] == 's')
 {
 const char *t = va_arg(list, const char *);
 if (t)
-for (int j = 0; t[j]; j++)
+for (j = 0; t[j]; j++)
 c += write(1, &t[j], 1);
 }
 else

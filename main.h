7c394202a0e51ba2aa6684f0deb581cd09752941
Ztote Achraf <achraf.ztote@gmail.com>
2023-11-14 16:04:33 -0800

    task1

--- main.h
+++ main.h
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 int _printf(const char *format, ...);
 int _inttoASC(int number, char *AC);
 int _printf1(const char *format, ...);
